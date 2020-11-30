0d13d9b8b8d8f1bdc79ccea6096d7ef05874750d
har00n-haider <72197495+har00n-haider@users.noreply.github.com>
2020-11-30 21:48:12 +0000

    Add files via upload
    Adding debugging with vscode, adding mouse click events

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+console.log(addNumbers(3,4))
+
 // Setting up 3d scene
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
@@ -7,11 +10,14 @@ renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
 // Setting up cube
-var geometry = new THREE.BoxGeometry( 1, 1, 1)
-var material = new THREE.MeshStandardMaterial( { color: 0x0000ff })
-var cube = new THREE.Mesh ( geometry, material )
-cube.position.set( 2, 1, -5 )
-scene.add( cube )
+function addCube(x,y,z, position){
+    var geometry = new THREE.BoxGeometry( x, y, z)
+    var material = new THREE.MeshStandardMaterial( { color: 0x0000ff })
+    var cube = new THREE.Mesh ( geometry, material )
+    cube.position.set(position.x, position.y, position.z)
+    scene.add( cube )
+    return cube;
+}
 
 // Light
 var ambientLight = new THREE.AmbientLight ( 0xffffff, 3)
@@ -20,10 +26,19 @@ var pointLight = new THREE.PointLight( 0xffffff, 3 );
 pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
 
+function someDeleteFunction(objectToDelete) {
+    // var selectedObject = scene.getObjectByName(object.name);
+    scene.remove(objectToDelete);
+    objectToDelete.geometry.dispose();
+    objectToDelete.material.dispose();
+    objectToDelete = undefined;
+}
 
-const raycaster = new THREE.Raycaster();
+function debugLog() {
+    console.log("mouse position: " + mouse.x.toFixed(2) + " " + mouse.y.toFixed(2));
+}
+
 const mouse = new THREE.Vector2();
-
 // calculate mouse position in normalized device coordinates
 // (-1 to +1) for both components
 function onMouseMove( event ) {
@@ -31,29 +46,31 @@ function onMouseMove( event ) {
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
-function renderMain() {
-    window.requestAnimationFrame(renderMain);
-
-    
-	raycaster.setFromCamera( mouse, camera );
+function onMouseDown(event){
+    raycaster.setFromCamera( mouse, camera );
 	const intersects = raycaster.intersectObjects( scene.children );
 	for ( let i = 0; i < intersects.length; i ++ ) {
-		intersects[ i ].object.material.color.set( 0xff0000 );
+
+        hitCube = intersects[ i ].object;
+        hitPos = hitCube.position;
+        someDeleteFunction(hitCube);
+        miniCube1 = addCube(0.3,0.3,0.3, hitPos)
     }
-    
-    debugLog();
-    cube.rotation.x += 0.04;
-    // //  cube.rotation.y += 0.02;
-    // //  cube.rotation.z += 0.01;
-
-	renderer.render( scene, camera );
-}
-
-function debugLog() {
-    console.log("mouse position: " + mouse.x.toFixed(2) + " " + mouse.y.toFixed(2));
 }
 
 window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener( 'mousedown', onMouseDown, false );
 
 
+cube = addCube(1, 1, 1, new THREE.Vector3(2, 1, -5));
+const raycaster = new THREE.Raycaster();
+// Main loop
+function renderMain() {
+    window.requestAnimationFrame(renderMain);
+    //debugLog();
+    cube.rotation.x += 0.04;
+    // //  cube.rotation.y += 0.02;
+    // //  cube.rotation.z += 0.01;
+	renderer.render( scene, camera );
+}
 renderMain()
