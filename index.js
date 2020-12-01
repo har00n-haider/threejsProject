2eb5895e3497b0c78d932ff0ce4b6945ab1467ab
haroon-haider <haroon.haider@3drepo.org>
2020-12-01 20:33:32 +0000

    updated vs launc json

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
+
+console.log(addNumbers(3,4))
+
+// Setting up 3d scene
 const scene = new THREE.Scene()
-
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-
+camera.position.z = 5
 const renderer = new THREE.WebGLRenderer({ antialias: true})
-
 renderer.setSize( window.innerWidth, window.innerHeight )
-
 document.body.appendChild( renderer.domElement )
 
+// Setting up cube
+function addCube(x,y,z, position){
+    var geometry = new THREE.BoxGeometry( x, y, z)
+    var material = new THREE.MeshStandardMaterial( { color: 0x0000ff })
+    var cube = new THREE.Mesh ( geometry, material )
+    cube.position.set(position.x, position.y, position.z)
+    scene.add( cube )
+    return cube;
+}
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1)
-
-// var material = new THREE.MeshBasicMaterial( { color: 0xffffff })
-
-var material = new THREE.MeshStandardMaterial( { color: 0xffffff })
-
-
-var cube = new THREE.Mesh ( geometry, material )
-
-scene.add( cube )
-
-// console.log("Cube position:")
-// console.log(cube.position)
-
-camera.position.z = 5
-
-
-// console.log("Camera position:")
-// console.log(camera.position)
-
-
-var ambientLight = new THREE.AmbientLight ( 0x0000ff, 9)
+// Light
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 3)
 scene.add( ambientLight )
-
 var pointLight = new THREE.PointLight( 0xffffff, 3 );
 pointLight.position.set( 25, 50, 25 );
 scene.add( pointLight );
 
-var geometry = new THREE.BoxGeometry( 3, 3, 3)
-var material = new THREE.MeshBasicMaterial( {
- color: "#dadada", wireframe: true, transparent: true
-})
-var wireframeCube = new THREE.Mesh ( geometry, material )
-scene.add( wireframeCube )
-
-function animate() {
- console.log("animation called")
-
- requestAnimationFrame( animate )
- cube.rotation.x += 0.04;
- cube.rotation.y += 0.02;
- cube.rotation.z += 0.01;
- renderer.render( scene, camera )
+function someDeleteFunction(objectToDelete) {
+    // var selectedObject = scene.getObjectByName(object.name);
+    scene.remove(objectToDelete);
+    objectToDelete.geometry.dispose();
+    objectToDelete.material.dispose();
+    objectToDelete = undefined;
 }
 
-animate()
+function debugLog() {
+    console.log("mouse position: " + mouse.x.toFixed(2) + " " + mouse.y.toFixed(2));
+}
+
+const mouse = new THREE.Vector2();
+// calculate mouse position in normalized device coordinates
+// (-1 to +1) for both components
+function onMouseMove( event ) {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function onMouseDown(event){
+    raycaster.setFromCamera( mouse, camera );
+	const intersects = raycaster.intersectObjects( scene.children );
+	for ( let i = 0; i < intersects.length; i ++ ) {
+
+        hitCube = intersects[ i ].object;
+        hitPos = hitCube.position;
+        someDeleteFunction(hitCube);
+        miniCube1 = addCube(0.3,0.3,0.3, hitPos)
+    }
+}
+
+window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener( 'mousedown', onMouseDown, false );
+
+
+cube = addCube(1, 1, 1, new THREE.Vector3(2, 1, -5));
+const raycaster = new THREE.Raycaster();
+// Main loop
+function renderMain() {
+    window.requestAnimationFrame(renderMain);
+    //debugLog();
+    cube.rotation.x += 0.04;
+    // //  cube.rotation.y += 0.02;
+    // //  cube.rotation.z += 0.01;
+	renderer.render( scene, camera );
+}
+renderMain()
---
+++ lib.js
@@ -0,0 +1,6 @@
+let msg = "Hello World!";
+const PI = 3.14; 
+ 
+function addNumbers(a, b){
+    return a + b;
+}
