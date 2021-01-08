ecc90858e578481f42b123ecf978482ae6f9f03d
haroon-haider <haroon.haider@3drepo.org>
2021-01-08 09:51:54 +0000

    adding files for the work on section planes

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import Debugger from './lib/gameEngine/utils/Debugger.js';
 import InputManager from './lib/gameEngine/utils/InputManager.js';
 import globals from './lib/gameEngine/Globals.js';
 import InfiniteGridHelper from './lib/InfiniteGridHelper.js';
+import {OBJLoader} from './lib/OBJLoader.js';
 // GameObjects
 import GameObjectManager from './lib/gameEngine/ecs/GameObjectManager.js';
 import AudioManager from './lib/gameEngine/utils/AudioManager.js';
+import {rand} from "./lib/gameEngine/utils/Utils.js";
+
 
 // Initial seup of scene, camera and lights
 function initialise() {
@@ -126,6 +129,121 @@ function setupGameObjects() {
   globals.gameObjectManager = new GameObjectManager();
   globals.inputManager = new InputManager(globals.renderer.domElement);
   globals.debugger = new Debugger(globals, document.getElementById('debugWrapper'));
+
+  // getting the section plane data from 3drepo
+  const url = 'https://api1.www.3drepo.io/api/HH/5169d210-43a0-11eb-8a99-47d28b0b42bd/issues/d161f560-4481-11eb-b892-037e1438a15d?key=b6b27d58c21d5ccaf1a0540d02cb2998';
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      display3drepoMesh(data.viewpoint.clippingPlanes[0]);
+    })
+    .catch((error) => {
+      console.log('Dammit');
+      console.log(error);
+    });
+}
+
+function display3drepoMesh(clippingPlane) {
+  console.log(clippingPlane);
+  // const normal = repoApiToThreejs(new THREE.Vector3(
+  //   clippingPlane.normal[0],
+  //   clippingPlane.normal[1],
+  //   clippingPlane.normal[2],
+  // ));
+
+  const distance = 5;
+  const planeNormal = new THREE.Vector3(1,1,1).normalize();
+  const newPos = new THREE.Vector3(planeNormal.x, planeNormal.y, planeNormal.z).multiplyScalar(distance);
+  const planeEq = {
+    a : planeNormal.x,
+    b : planeNormal.y,
+    c : planeNormal.z,
+    d : distance
+  }
+
+  // the dice model
+  const loader = new OBJLoader();
+  loader.load(
+    // resource URL
+    'assets/dado.obj',
+    // called when resource is loaded
+    function ( object ) {
+      object.lookAt(new THREE.Vector3(0,1,0));
+      object.rotateOnWorldAxis(new THREE.Vector3(0,1,0), -90 * Math.PI/180);
+      globals.scene.add( object );
+    },
+    // called when loading is in progresses
+    function ( xhr ) {
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    // called when loading has errors
+    function ( error ) {
+      console.log( 'An error happened' );
+    }
+  );
+
+  // plane
+  const plane = globals.gameObjectManager.createGameObject(globals.scene, 'plane');
+  const planeGeometry = new THREE.PlaneGeometry(10, 10);
+  const planeMaterial = new THREE.MeshStandardMaterial({ color: 'green', side: THREE.DoubleSide });
+  const planeGeo = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.transform.add(planeGeo);
+  plane.transform.lookAt(planeNormal);
+  plane.transform.position.set(newPos.x, newPos.y, newPos.z);
+
+  //let perpVector = new THREE.Vector3(0, planeNormal.z, -planeNormal.y);
+  let perpVector = calculateRandPerpVector(planeNormal, 10);
+  let normalArrow = new THREE.ArrowHelper(planeNormal,newPos, 3, 0xff0000);
+  let perpArrow = new THREE.ArrowHelper(perpVector, newPos, 3, 'purple');
+  console.log(perpVector.angleTo(planeNormal)*(180/Math.PI));
+
+  globals.scene.add(normalArrow);
+  globals.scene.add(perpArrow);
+
+  // points 
+  const points = globals.gameObjectManager.createGameObject(globals.scene, 'plane');
+  var dotGeometry = new THREE.Geometry();
+  dotGeometry.vertices = calculateRandomPointsOnPlane(planeEq, 100, 5);
+
+  var dotMaterial = new THREE.PointsMaterial( { size: 3, sizeAttenuation: false, color: 'blue' } );
+  var dots = new THREE.Points( dotGeometry, dotMaterial );
+  points.transform.add(dots);
+
+
+
+  // // scotch tape
+  // const scotchTape = globals.gameObjectManager.createGameObject(globals.scene, 'scotchTape');
+  // const tapegeometry = new THREE.CylinderGeometry(25, 25, 10, 64);
+  // const tapeMaterial = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+  // const tapeCylinder = new THREE.Mesh(tapegeometry, tapeMaterial);
+  // scotchTape.transform.add(tapeCylinder);
+}
+
+function repoApiToThreejs(vector) {
+  return new THREE.Vector3(
+    vector.z,
+    vector.y,
+    vector.x,
+  );
+}
+
+
+function calculateRandomPointsOnPlane(planeEq, numPoints, range){
+  let randArr = [];
+  for (let i = 0; i < numPoints; i++) {
+    let xRand = rand(-range, range);
+    let yRand = rand(-range, range);
+    let z = (planeEq.d - planeEq.a*xRand - planeEq.b*yRand)/planeEq.c;
+    randArr.push(new THREE.Vector3(xRand, yRand,z))
+  }
+  return randArr;
+}
+
+function calculateRandPerpVector(vector, range){
+  let xRand = rand(-range, range);
+  let yRand = rand(-range, range);
+  let z = (-vector.x*xRand -vector.y*yRand)/vector.z;
+  return new THREE.Vector3(xRand, yRand, z);
 }
 
 initialise();
