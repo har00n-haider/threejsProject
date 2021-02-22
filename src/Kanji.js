3ea48ae864fadd0ee4d2afbe1a3ffa8bd25135cb
haroon-haider <haroon.haider@3drepo.org>
2021-02-22 15:31:44 +0000

    adding code to animate a line as its input

--- src/Kanji.js
+++ src/Kanji.js
@@ -4,14 +4,57 @@ import * as Utils from '../lib/gameEngine/utils/Utils.js'
 import * as m from '../lib/THREE.MeshLine.js';
 import { MeshLine, MeshLineMaterial } from '../lib/THREE.MeshLine.js';
 
-let line = {};
-
 //#region input kanji
+let linePosArr = []
+let line;
+const MAX_POINTS = 500;
+let drawCount;
+
+function updatePositions() {
+	const positions = line.geometry.attributes.position.array;
+	let x, y, z, index;
+	x = y = z = index = 0;
+	for ( let i = 0, l = MAX_POINTS; i < l; i ++ ) {
+		positions[ index ++ ] = x;
+		positions[ index ++ ] = y;
+		positions[ index ++ ] = z;
+		x += ( Math.random() - 0.5 ) * 30;
+		y += ( Math.random() - 0.5 ) * 30;
+		z += ( Math.random() - 0.5 ) * 30;
+	}
+}
+
+function getLine(){
+    // geometry
+	const geometry = new THREE.BufferGeometry();
+	// attributes
+	const positions = new Float32Array( MAX_POINTS * 3 ); // 3 vertices per point
+	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+	// drawcalls
+	drawCount = 2; // draw the first 2 points, only
+	geometry.setDrawRange( 0, drawCount );
+	// material
+	const material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+	// line
+	line = new THREE.Line( geometry,  material );
+}
+
+function update(){
+	drawCount = ( drawCount + 1 ) % MAX_POINTS;
+	line.geometry.setDrawRange( 0, drawCount );
+	if ( drawCount === 0 ) {
+		// periodically, generate new data
+		updatePositions();
+		line.geometry.attributes.position.needsUpdate = true; // required after the first render
+		line.material.color.setHSL( Math.random(), 1, 0.5 );
+	}
+}
 
 function init(){
-
+    const line = makeLine();
+    globals.add(line);
     globals.inputManager.moveEvent.regCb((p)=>{
-
+        linePosArr.push(p.pointerPos);
     });
 }
 
