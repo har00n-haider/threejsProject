d779269c36bfa7c35c1dc659b7c928799bea28c6
haroon-haider <haroon.haider@3drepo.org>
2021-04-30 11:33:14 +0100

    beginnings of a the attempt to try and get a database of kanji letters into the game

--- kanjiDb/kanjiDbGen.py
+++ kanjiDb/kanjiDbGen.py
@@ -1,61 +1,61 @@
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
-import codecs
-
-dicPath = 'kanjiDic2-cutdown.xml'
-knjPath = 'kanjivg-cutdown.xml'
-outPath = 'kanjiDb.xml'
-
-dicTree = ET.parse(dicPath)
-dicRoot = dicTree.getroot()
-
-knjTree = ET.parse(knjPath)
-knjRoot = knjTree.getroot()
-
-def prettify(rootElem, strip=True):
-    """
-    Return a pretty-printed XML string for the Element.
-    """
-    if strip:
-      for elem in rootElem.iter():
-        if(elem.text):
-            elem.text = elem.text.strip()
-        if(elem.tail):
-            elem.tail = elem.tail.strip()
-    rough_string = ET.tostring(rootElem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent=" ")
-
-def writeFile(elem, path, pretty=True):
-  # create a new XML file with the results
-  mydata = ''
-  if pretty:
-    mydata = prettify(elem)
-  else:
-    tree = ET.ElementTree(elem)
-    tree.write(outPath, encoding='utf-8')
-  with open(path, 'w', encoding='utf8') as myfile:
-    myfile.write(mydata)
-
-# create the o/p db structure
-kDbRoot = ET.Element('kanjiDb')
-
-dicEntry = dicRoot[1]
-knjEntry = knjRoot[0]
-
-x = u'桀'
-
-y =  1
-
-# kDbEntry = ET.SubElement(dicEntry, knjEntry)
-# kDbEntry = ET.SubElement(kDbRoot, dicEntry)
-
-ET.SubElement(dicEntry, 'kanji').extend(knjEntry)
-ET.SubElement(kDbRoot, 'character').extend(dicEntry)
+from lxml import etree
 
 
-writeFile(kDbRoot, outPath)
+root = etree.Element('root')
+
+etree.SubElement(root, 'child1')
+etree.SubElement(root, 'child2')
+
+rootStr = etree.tostring(root, pretty_print=True)
+
+child = root[0]
+
+x = 0
 
 
-x = 1
+# dicPath = 'kanjiDic2-cutdown.xml'
+# knjPath = 'kanjivg-cutdown.xml'
+# outPath = 'kanjiDb.xml'
 
+# dicTree = ET.parse(dicPath)
+# dicRoot = dicTree.getroot()
+
+# knjTree = ET.parse(knjPath)
+# knjRoot = knjTree.getroot()
+
+
+# def findEntryInDic(kanji, dic):
+
+#   return
+
+# # create the o/p db structure
+# kDbRoot = ET.Element('kanjiDb')
+
+# dicEntry = dicRoot[1]
+# knjEntry = knjRoot[0]
+
+# # kDbEntry = ET.SubElement(dicEntry, knjEntry)
+# # kDbEntry = ET.SubElement(kDbRoot, dicEntry)
+
+# ET.SubElement(dicEntry, 'kanji').extend(knjEntry)
+# ET.SubElement(kDbRoot, 'character').extend(dicEntry)
+
+# # Set 1
+# kanjiList = [
+#   '一',
+#   '九',
+#   '七',
+#   '十',
+#   '人',
+#   '二',
+#   '入',
+#   '八',
+#   '下',
+#   '口',
+#   '三',
+#   '山'
+#   ]
+
+# kanjiElem = findEntryInDic(kanjiList[0], dicRoot)
+
+# writeFile(kDbRoot, outPath)
